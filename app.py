0145fa5a09a626ba4c210e0e296c783e15d51a19
Jervisol <jervisol@hotmail.com>
2025-12-12 18:05:01 +0800

    feature timeline chart

--- app.py
+++ app.py
@@ -394,4 +394,4 @@ async def upload_video(request: Request):
 if __name__ == '__main__':
     import uvicorn
     # uvicorn.run(app, host='0.0.0.0', port=5000, reload=True)
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=5055, reload=True)
